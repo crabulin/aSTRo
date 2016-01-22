c7ebf7dc2911d88f5ee75efe0d1200dc67d60e19
david <david@celeste>
2016-01-22 07:19:04 +0100

    za

---
+++ aSTRo/src/Zelda.java
@@ -0,0 +1,15 @@
+
+public class Zelda extends Entite {
+
+	public Zelda(int x, int y) {
+		super(x, y);
+	}
+
+	int droiteGauche = 1;
+	long pourcentage;
+	double vitesse = 0.5;   //cases par seconde
+	
+	public void update(long dt){
+		
+	}
+}
